5f73e0f5e7d76b4e906fc855bf9494a459d0c9d8
Chetan Agarwal <76930533+ChetansMittal@users.noreply.github.com>
2021-10-10 09:41:39 +0530

    Update connect1.php

--- Feedback/connect1.php
+++ Feedback/connect1.php
@@ -11,7 +11,7 @@
 	$complaint = $_POST['complaint'];
 
 	// Database connection
-	$conn = new mysqli('localhost','root','','test');
+	$conn = new mysqli('localhost','root','','feed_form');
 	if($conn->connect_error){
 		echo "$conn->connect_error";
 		die("Connection Failed : ". $conn->connect_error);
